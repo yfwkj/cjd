687031bd64f46085e90bb7f0939d9d993efb6666
baqgyytdkj123 <baqgyytdkj@126.com>
2020-06-30 11:46:14 +0800

    更新实体类的验证

---
+++ src/main/java/com/yfw/chengjiaodan/config/RestTemplateConfig.java
@@ -0,0 +1,34 @@
+package com.yfw.chengjiaodan.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName: RestTemplateConfig
+ * @Description: TODO
+ * @Author: Chenyz
+ * @Date: 2020/6/30 10:27
+ * @Version: 1.0
+ **/
+@Configuration
+public class RestTemplateConfig {
+
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);//ms
+        factory.setConnectTimeout(15000);//ms
+        return factory;
+
+    }
+}
--- src/main/java/com/yfw/chengjiaodan/controller/UpdateController.java
+++ src/main/java/com/yfw/chengjiaodan/controller/UpdateController.java
@@ -14,24 +14,29 @@ import net.coobird.thumbnailator.makers.ThumbnailMaker;
 import net.coobird.thumbnailator.util.ThumbnailatorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.Resource;
+import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
-import java.io.File;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -53,13 +58,20 @@ public class UpdateController extends BaseApiService<JSONObject> {
     @Value("${spring.servlet.multipart.location}")
     private String location;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/upload")
-    public BaseResponse<JSONObject> upload(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request) {
+    public BaseResponse<JSONObject> upload(@RequestBody MultipartFile file, HttpServletRequest request) {
+
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmssSS");
         String currentTimeMillis = dateFormat.format(new Date(System.currentTimeMillis()));
         try {
-            multipartFile.transferTo(new File(currentTimeMillis + ".jpg"));
-            String path = request.getRequestURL() + "/" + multipartFile.getOriginalFilename();
+            if(null == file){
+                return setResultError("上传失败，上传附件不能为空");
+            }
+            file.transferTo(new File(currentTimeMillis + ".jpg"));
+            String path = request.getRequestURL() + "/" + currentTimeMillis + ".jpg";
             //源图片路径
             String sourceFile = location + "/" + currentTimeMillis + ".jpg";
             //缩放比例
@@ -68,16 +80,31 @@ public class UpdateController extends BaseApiService<JSONObject> {
             Float quality = 1.0f;
             //图片输出路径
             String outFilePath = location + "/" + currentTimeMillis + "thumbnails.jpg";
-            ThumbnailsUtil.setThumbnails(sourceFile,scale,quality,outFilePath);
+            //缩略图
+            //ThumbnailsUtil.setThumbnails(sourceFile,scale,quality,outFilePath);
             JSONObject value = new JSONObject();
+
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+            MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();
+            //log.info(multipartFile.getOriginalFilename());
+            parts.add("file", file.getResource());
+            HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(parts, headers);
+            ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity("http://www.fangdianwang.com:8082/uploadfile/upload", httpEntity, String.class);
+            String replace = stringResponseEntity.getBody();
+
+
+
             value.put("src",path);
-            value.put("thumbnail","");
+            value.put("thumbnail",outFilePath);
+
             return setResult(200, "上传成功", value);
         } catch (Exception e) {
             e.printStackTrace();
+            return setResultError("上传失败");
         }
 
-        return setResultError("上传失败");
     }
 
 
