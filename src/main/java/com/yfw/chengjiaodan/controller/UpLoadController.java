7dacb59d988d58f7fe89db1f9818598fb560a155
baqgyytdkj123 <baqgyytdkj@126.com>
2020-06-30 16:38:21 +0800

    缩略图 原图上传

--- src/main/java/com/yfw/chengjiaodan/controller/UpLoadController.java
+++ src/main/java/com/yfw/chengjiaodan/controller/UpLoadController.java
@@ -1,7 +1,6 @@
 package com.yfw.chengjiaodan.controller;
 
 
-
 import com.alibaba.fastjson.JSONObject;
 import com.yfw.chengjiaodan.base.BaseApiService;
 import com.yfw.chengjiaodan.base.BaseResponse;
@@ -33,7 +32,6 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 
 
-
 /**
  * @ClassName: IndexController
  * @Description: TODO
@@ -46,64 +44,65 @@ import java.io.*;
 @Slf4j
 public class UpLoadController extends BaseApiService<JSONObject> {
 
-    //@Value("${spring.servlet.multipart.location}")
-    //private String location;
-
     @Autowired
     private RestTemplate restTemplate;
+
     @RequestMapping("/upload")
     public BaseResponse<JSONObject> upload(@RequestBody MultipartFile file, HttpServletRequest request) {
+
+        if (file == null)
+            return setResultError("上传失败，上传附件不能为空");
         JSONObject value = new JSONObject();
-        value.put("src", "src");
-        value.put("thumbnail", "thumbnail");
+        ResponseEntity<String> stringResponseEntity = null;
 
+        HttpHeaders headers1 = new HttpHeaders();
+        headers1.setContentType(MediaType.MULTIPART_FORM_DATA);
+        MultiValueMap<String, Object> parts1 = new LinkedMultiValueMap<>();
+        //log.info(multipartFile.getOriginalFilename());
+        parts1.add("file", file.getResource());
+//        parts.add("file", thumbnailsFile.getResource());
+        HttpEntity<MultiValueMap<String, Object>> httpEntity1 = new HttpEntity<>(parts1, headers1);
+        stringResponseEntity = restTemplate.postForEntity("http://www.fangdianwang.com:8082/uploadfile/upload", httpEntity1, String.class);
+        JSONObject data1 = JSONObject.parseObject(stringResponseEntity.getBody());
+        JSONObject src1 = JSONObject.parseObject(data1.get("data") + "");
+
+        InputStream inputStream = null;
+        File localFile = null;
+        MultipartFile thumbnailsFile = null;
+
+        //缩放比例
+        Double scale = 0.1;
+        //图片质量
+        Float quality = 1.0f;
+        //图片输出路径
+        String outFilePath = "E:/sundry/uploadfile/thumbnails.jpg";
+
+        try {
+
+            Thumbnails.of(file.getInputStream()).scale(0.1f).outputQuality(1.0f).toFile(outFilePath);
+            localFile = new File(outFilePath);
+            inputStream = new FileInputStream(localFile);
+            System.out.println(localFile.getName());
+            thumbnailsFile = new MockMultipartFile(localFile.getName(), localFile.getName(), file.getContentType(), inputStream);
+            thumbnailsFile.transferTo(new File("E:/sundry/uploadfile/test.jpg"));
+
+            HttpHeaders headers2 = new HttpHeaders();
+            headers2.setContentType(MediaType.MULTIPART_FORM_DATA);
+            MultiValueMap<String, Object> parts2 = new LinkedMultiValueMap<>();
+            //log.info(multipartFile.getOriginalFilename());
+            parts2.add("file", thumbnailsFile.getResource());
+            HttpEntity<MultiValueMap<String, Object>> httpEntity2 = new HttpEntity<>(parts2, headers2);
+            stringResponseEntity = restTemplate.postForEntity("http://www.fangdianwang.com:8082/uploadfile/upload", httpEntity2, String.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JSONObject data2 = JSONObject.parseObject(stringResponseEntity.getBody());
+        JSONObject src2 = JSONObject.parseObject(data2.get("data") + "");
+
+        value.put("src", src1.get("src"));
+        value.put("thumbnail", src2.get("src"));
         return setResult(200, "上传成功", value);
-
     }
-
-//    @RequestMapping("/upload11")
-//    public BaseResponse<JSONObject> upload11(@RequestBody MultipartFile file, HttpServletRequest request) {
-//        if (file == null)
-//            return setResultError("上传失败，上传附件不能为空");
-//        InputStream in = null;
-//        File file1 = null;
-//        MultipartFile thumbnailsFile = null;
-//        //缩放比例
-//        Double scale = 0.1;
-//        //图片质量
-//        Float quality = 1.0f;
-//        //图片输出路径
-//        String outFilePath = "E:/sundry/uploadfile/thumbnails.jpg";
-//        try {
-//            Thumbnails.of(file.getInputStream()).scale(0.1f).outputQuality(1.0f).toFile(outFilePath);
-//            file1 = new File(outFilePath);
-//            in = new FileInputStream(file1);
-//            thumbnailsFile = new MockMultipartFile(file1.getName(),in);
-//            thumbnailsFile.transferTo(new File("E:/sundry/uploadfile/test.jpg"));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-//        MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();
-//        //log.info(multipartFile.getOriginalFilename());
-//        parts.add("file", file.getResource());
-//        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(parts, headers);
-//        ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity("http://www.fangdianwang.com:8082/uploadfile/upload", httpEntity, String.class);
-//        String replace = stringResponseEntity.getBody();
-//
-//        return null;
-////        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmssSS");
-////        String currentTimeMillis = dateFormat.format(new Date(System.currentTimeMillis()));
-////            JSONObject value = new JSONObject();
-////            value.put("src",path);
-////            value.put("thumbnail",outFilePath);
-//
-////            return setResult(200, "上传成功", value);
-////
-//
-//    }
 
 
 }
