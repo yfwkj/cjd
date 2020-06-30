bfe1eaf5fe1a4e0bbc44ebf4161fd498aac77f7b
baqgyytdkj123 <baqgyytdkj@126.com>
2020-06-30 10:01:19 +0800

    更新实体类

--- src/main/java/com/yfw/chengjiaodan/mapper/entity/YfwCjdEntity.java
+++ src/main/java/com/yfw/chengjiaodan/mapper/entity/YfwCjdEntity.java
@@ -36,6 +36,7 @@ public class YfwCjdEntity {
     @NotBlank(message = "销售员姓名不能为空")
     private String seller;
 
+    //销售员电话必填
     @NotBlank(message = "销售员手机号码不能为空")
 //    @Size(min = 11, max = 11, message = "销售员手机号码长度不正确")
 //    @Pattern(regexp = "^(((13[0-9])|(14[579])|(15([0-3]|[5-9]))|(16[6])|(17[0135678])|(18[0-9])|(19[89]))\\d{8})$", message = "销售员手机号格式错误")
@@ -61,7 +62,7 @@ public class YfwCjdEntity {
     private String cuserSource;
     private String fxName;
 
-    @NotBlank(message = "分销员手机号码不能为空")
+//    @NotBlank(message = "分销员手机号码不能为空")
 //    @Size(min = 11, max = 11, message = "分销员手机号码长度不正确")
 //    @Pattern(regexp = "^(((13[0-9])|(14[579])|(15([0-3]|[5-9]))|(16[6])|(17[0135678])|(18[0-9])|(19[89]))\\d{8})$", message = "分销员手机号格式错误")
     private String fxMobi;
