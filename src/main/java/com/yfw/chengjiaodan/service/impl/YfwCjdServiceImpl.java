d07781203896f46552093b97433d41941e6068b6
baqgyytdkj123 <baqgyytdkj@126.com>
2020-07-03 16:04:18 +0800

    成交单 验证签约日期

--- src/main/java/com/yfw/chengjiaodan/service/impl/YfwCjdServiceImpl.java
+++ src/main/java/com/yfw/chengjiaodan/service/impl/YfwCjdServiceImpl.java
@@ -10,6 +10,7 @@ import com.yfw.chengjiaodan.mapper.YfwCjdMapper;
 import com.yfw.chengjiaodan.mapper.YfwConfigMapper;
 import com.yfw.chengjiaodan.mapper.entity.YfwCjdEntity;
 import com.yfw.chengjiaodan.service.YfwCjdService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * @ClassName: YfwCjdServiceImpl
@@ -47,6 +49,17 @@ public class YfwCjdServiceImpl extends BaseApiService<YfwCjdEntity> implements Y
         //.验证对象
         if (yfwCjdEntity == null) {
             return setResultError(Constants.HTTP_RES_CODE_201, "参数对象为空");
+        }
+        if(StringUtils.isEmpty(yfwCjdEntity.getQyDate())){
+            yfwCjdEntity.setQyDate(null);
+        }
+        if(!StringUtils.isEmpty(yfwCjdEntity.getQyDate())){
+            String qyDate = yfwCjdEntity.getQyDate();
+            Boolean result = Pattern.matches(
+                    "^(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29)$",qyDate);
+            if(!result){
+                return setResultError("签约日期错误");
+            }
         }
         //查询yfwConfig表里的value 字段值
         String yfwConfigValue = yfwConfigMapper.selectYfwConfigValue();
